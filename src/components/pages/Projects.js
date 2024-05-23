3e3aaa8571084335f8c501782b468d3d9b4e6b0a
lucasgomes14 <lucazmatehus14@gmail.com>
2024-05-23 14:52:38 -0300

    Rescuing projects from the database

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,16 +5,34 @@ import Container from "../layout/Container"
 import LinkButton from "../layout/LinkButton"
 
 import styles from "./Projects.module.css"
+import ProjectCard from "../project/ProjectCard"
+import { useState, useEffect } from "react"
 
 function Projects() {
+
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
 
     let message = ""
 
-    if(location.state) {
+    if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch("http://localhost:5000/projects", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                setProjects(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
 
     return (
         <div className={styles.project_container}>
@@ -24,7 +42,15 @@ function Projects() {
             </div>
             {message && <Message type="success" msg={message} />}
             <Container customClass="start">
-                <p>Projetos...</p>
+                {projects.length > 0 && projects.map((project) => (
+                    <ProjectCard
+                        id={project.id}
+                        name={project.name}
+                        budget={project.budget}
+                        category={project.category.name}
+                        key={project.id}
+                    />
+                ))}
             </Container>
         </div>
     )
